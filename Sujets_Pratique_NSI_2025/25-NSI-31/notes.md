4377ee1c99c4cbdf116bfcdbfd04c7b19dc9b417
Antoine <antoine.coste@e.rascol.net>
2025-05-11 17:49:23 +0200

    Sujet 31

---
+++ Sujets_Pratique_NSI_2025/25-NSI-31/notes.md
@@ -0,0 +1,3 @@
+# Exercice 1:
+Parcourir la liste jusqu'à ce que le motif ne rentre plus
+Booléen à True qui se met à false si une lettre ne correspond pas.
--- Sujets_Pratique_NSI_2025/25-NSI-31/sujet-31.py
+++ Sujets_Pratique_NSI_2025/25-NSI-31/sujet-31.py
@@ -1,19 +1,36 @@
+# Exercice 1
+def recherche_motif(motif:str, texte:str) -> list[int]:
+    occurences = []
+    for i in range(len(texte)):
+        is_in = True
+        if len(motif) > len(texte) - i:
+            return occurences
+        for j in range(len(motif)):
+            if motif[j] != texte[i+j]:
+                is_in = False
+        if is_in:
+            occurences.append(i)
+    return occurences
+
+print(recherche_motif("ab", ""))
+
+# Exercice 2
 def parcours(adj, x, acc):
     '''Réalise un parcours en profondeur récursif
     du graphe donné par les listes d'adjacence adj 
     depuis le sommet x en accumulant les sommets
     rencontrés dans acc'''
-    if x ...: 
+    if x not in acc: 
         acc.append(x)
-        for y in ...: 
-            parcours(adj, ...) 
+        for y in adj[x]: 
+            parcours(adj, y, acc) 
 
 def accessibles(adj, x):
     '''Renvoie la liste des sommets accessibles dans le
     graphe donné par les listes d'adjacence adj depuis
     le sommet x.'''
     acc = []
-    parcours(adj, ...) 
+    parcours(adj, x, acc) 
     return acc
-
+ 
 
