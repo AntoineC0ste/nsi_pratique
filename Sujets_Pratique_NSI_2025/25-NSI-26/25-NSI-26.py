831b9679385d5ba45647cb0d4e38188e47be06ec
Antoine <antoine.coste@e.rascol.net>
2025-05-11 17:49:32 +0200

    26

--- Sujets_Pratique_NSI_2025/25-NSI-26/25-NSI-26.py
+++ Sujets_Pratique_NSI_2025/25-NSI-26/25-NSI-26.py
@@ -1,3 +1,21 @@
+# Exercice 1
+def ajoute_dictionnaire(d1:dict, d2:dict) -> dict:
+    '''Ajoute les deux dictionnaires en paramètre'''
+    d3 = {}
+    for cle, valeur in d1.items():
+        if cle in d2:
+            d3[cle] = valeur + d2[cle]
+        else:
+            d3[cle] = valeur
+    for cle, valeur in d2.items():
+        if cle not in d3:
+            d3[cle] = valeur 
+    
+    return d3
+
+print(ajoute_dictionnaire({1: 5, 2: 7}, {2: 9, 3: 11}))
+
+# Exercice 2
 from random import randint
 
 def nombre_coups():
@@ -9,13 +27,13 @@ def nombre_coups():
     nombre_cases_vues = 1
     cases_vues[0] = True
     case_en_cours = 0
-    n = ... 
-    while ... < ...: 
+    n = 0
+    while nombre_cases_vues < nombre_cases: 
         x = randint(1, 6)
-        case_en_cours = (case_en_cours + ...) % ... 
-        if ...: 
+        case_en_cours = (case_en_cours + x) % nombre_cases 
+        if not cases_vues[case_en_cours]: 
             cases_vues[case_en_cours] = True
-            nombre_cases_vues = ... 
-        n = ... 
+            nombre_cases_vues = nombre_cases_vues + 1 
+        n = n + 1 
     return n
 
