c01b49633cbdc77dc8696f85459cf12d093a8670
Antoine <antoine.coste@e.rascol.net>
2025-05-17 10:07:30 +0200

    38

--- Sujets_Pratique_NSI_2025/25-NSI-38/25-NSI-38.py
+++ Sujets_Pratique_NSI_2025/25-NSI-38/25-NSI-38.py
@@ -1,12 +1,30 @@
+# Exercice 1
+def moyenne(tab:list[float]) -> float:
+    numerateur = 0
+    denominateur = len(tab)
+    for nombre in tab:
+        numerateur += nombre
+    
+    return numerateur/denominateur
+
+assert moyenne([1.0]) == 1.0
+print(moyenne([1.0, 2.0, 4.0]))
+
+
+# Exercice 2
 def binaire(a):
     '''convertit un nombre entier a en sa representation 
     binaire sous forme de chaine de caractères.'''
     if a == 0:
-        return ... 
-    bin_a = ... 
-    while ... : 
-        bin_a = ... + bin_a 
-        a = ... 
+        return '0'
+    bin_a = ''
+    while a > 0 : 
+        bin_a = str(a % 2) + bin_a 
+        a = a // 2
     return bin_a
 
 
+assert binaire(83) == '1010011'
+assert binaire(6) == '110'
+assert binaire(127) == '1111111'
+assert binaire(0) == '0'
