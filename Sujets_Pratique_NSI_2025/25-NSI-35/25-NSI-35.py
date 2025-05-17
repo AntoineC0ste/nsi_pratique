8e7a9b1c56f79acf79fe7c02bb476c9b9e974c4e
Antoine <antoine.coste@e.rascol.net>
2025-05-17 10:19:21 +0200

    35

--- Sujets_Pratique_NSI_2025/25-NSI-35/25-NSI-35.py
+++ Sujets_Pratique_NSI_2025/25-NSI-35/25-NSI-35.py
@@ -1,3 +1,18 @@
+# Exercice 1
+def max_dico(dico:dict[str:int]) -> tuple[str,int]:
+    assert dico != {}
+    maxi = 0 # On peut se permettre de mettre 0 car il s'agit d'entiers naturels
+    nom_maxi = ''
+    for nom, likes in dico.items():
+        if likes > maxi:
+            maxi = likes
+            nom_maxi = nom
+    
+    return (nom_maxi, maxi)
+
+assert max_dico({ 'Bob': 102, 'Ada': 201, 'Alice': 103, 'Tim': 50 }) == ("Ada", 201)
+
+# Exercice 2
 class Pile:
     """Classe définissant une structure de pile."""
     def __init__(self):
@@ -21,15 +36,18 @@ class Pile:
 
 def eval_expression(tab):
     p = Pile()
-    for ... in tab: 
-        if element != '+' ... element != '*': 
-            p.empiler(...) 
+    for element in tab: 
+        if element != '+' and element != '*': 
+            p.empiler(element) 
         else:
-            if element == ...: 
-                resultat = ... + ... 
+            if element == '+': 
+                resultat = p.depiler() + p.depiler() 
             else:
-                resultat = ... 
-            p.empiler(...) 
-    return ... 
+                resultat = p.depiler() * p.depiler()
+            p.empiler(resultat) 
+    return resultat
 
 
+assert  eval_expression([2, 3, '+', 5, '*']) == 25
+assert  eval_expression([1, 2, '+', 3, '*']) == 9
+assert  eval_expression([1, 2, 3, '+', '*']) == 5
