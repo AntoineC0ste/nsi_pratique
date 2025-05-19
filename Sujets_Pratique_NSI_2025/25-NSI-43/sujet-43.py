d1dee1d83719dbb227d0bb606cdd054809631b36
Antoine <antoine.coste@e.rascol.net>
2025-05-19 11:32:02 +0200

    43

--- Sujets_Pratique_NSI_2025/25-NSI-43/sujet-43.py
+++ Sujets_Pratique_NSI_2025/25-NSI-43/sujet-43.py
@@ -1,3 +1,16 @@
+# Exercice 1
+def couples_consecutifs(tab:list[int]):
+    couples = []
+    for i in range(len(tab)-1):
+        if tab[i] + 1 == tab[i+1]:
+            couples.append((tab[i], tab[i+1]))
+
+    return couples
+
+assert  couples_consecutifs([1, 4, 3, 5]) == []
+assert  couples_consecutifs([5, 1, 2, 3, 8, -5, -4, 7]) == [(1, 2), (2, 3), (-5, -4)]
+
+# Exercice 2
 def colore_comp1(M, i, j, val):
     if M[i][j] != 1:
         return
@@ -6,11 +19,13 @@ def colore_comp1(M, i, j, val):
 
     if i-1 >= 0: # propage en haut
         colore_comp1(M, i-1, j, val)
-    if ... < len(M): # propage en bas 
-        colore_comp1(M, ..., j, val) 
-    if ...: # propage à gauche 
-        colore_comp1(M, ..., ..., val) 
-    if ...: # propage à droite 
-        ...
+    if i+1 < len(M): # propage en bas 
+        colore_comp1(M, i+1, j, val) 
+    if j-1 >= 0: # propage à gauche 
+        colore_comp1(M, i, j-1, val) 
+    if j+1 < len(M[0]): # propage à droite 
+        colore_comp1(M, i, j+1, val) 
 
-
+M = [[0, 0, 1, 0], [0, 1, 0, 1], [1, 1, 1, 0], [0, 1, 1, 0]]
+colore_comp1(M, 2, 1, 3)
+assert M == [[0, 0, 1, 0], [0, 3, 0, 1], [3, 3, 3, 0], [0, 3, 3, 0]]
