8a4cc65f97ffa8a48ef6b11b5ab9180cc0e51e1e
Antoine <antoine.coste@e.rascol.net>
2025-05-19 11:15:12 +0200

    27

--- Sujets_Pratique_NSI_2025/25-NSI-27/25-NSI-27.py
+++ Sujets_Pratique_NSI_2025/25-NSI-27/25-NSI-27.py
@@ -1,12 +1,24 @@
+# Exercice 1
+def verifie(tab:list[float]) -> bool:
+    for i in range(len(tab)-1):
+        if tab[i] > tab[i+1]:
+            return False
+    return True
+
+assert verifie([0, 5, 8, 8, 9])
+assert  not verifie([8, 12, 4])
+
+
+# Exercice 2
 def depouille(urne):
     '''prend en paramètre une liste de suffrages et renvoie un 
     dictionnaire avec le nombre de voix pour chaque candidat'''
-    resultat = ... 
+    resultat = {}
     for bulletin in urne:
-        if ...: 
+        if bulletin in resultat.keys(): 
             resultat[bulletin] = resultat[bulletin] + 1
         else:
-            ...
+            resultat[bulletin] = 1
     return resultat
 
 def vainqueurs(election):
@@ -14,9 +26,9 @@ def vainqueurs(election):
     pour chaque candidat et renvoie la liste des vainqueurs'''
     nmax = 0
     for candidat in election:
-        if ... > ... : 
-            nmax = ... 
-    liste_finale = [ nom for nom in election if ... ] 
-    return ... 
+        if election[candidat] > nmax : 
+            nmax = election[candidat]
+    liste_finale = [ nom for nom in election if election[nom] == nmax ] 
+    return liste_finale
 
 
