b123ddbec778159850289ff115749ceb0749e5b2
Antoine <antoine.coste@e.rascol.net>
2025-05-20 09:16:24 +0200

    6

--- Sujets_Pratique_NSI_2025/25-NSI-6/sujet-6.py
+++ Sujets_Pratique_NSI_2025/25-NSI-6/sujet-6.py
@@ -1,3 +1,28 @@
+# Exercice 1
+def liste_puissances(a:int, n:int) -> list:
+    rslt = []
+    p = 1
+    for i in range(n):
+        p = p*a
+        rslt.append(p)
+    
+    return rslt
+
+assert liste_puissances(3, 5) == [3, 9, 27, 81, 243]
+
+def liste_puissances_borne(a:int, borne:int):
+    rslt = []
+    p = 1
+    
+    while p < borne:
+        p = p * a
+        rslt.append(p)
+
+    return rslt[:-1]
+
+assert  liste_puissances_borne(2, 16) == [2,4,8]
+
+# Exercice 2
 dico = {"A": 1, "B": 2, "C": 3, "D": 4, "E": 5, "F": 6,
         "G": 7, "H": 8, "I": 9, "J": 10, "K": 11, "L": 12,
         "M": 13, "N": 14, "O": 15, "P": 16, "Q": 17,
@@ -11,12 +36,14 @@ def codes_parfait(mot):
     - code_concatene est le code des lettres du mot concaténées ;
     - mot_est_parfait est un booléen indiquant si le mot est parfait."""
     code_concatene = ""
-    code_additionne = ... 
+    code_additionne = 0
     for c in mot:
-        code_concatene = code_concatene + ... 
-        code_additionne = code_additionne + ... 
+        code_concatene = code_concatene + str(dico[c])
+        code_additionne = code_additionne + dico[c] 
     code_concatene = int(code_concatene)
-    mot_est_parfait = ... 
+    mot_est_parfait = code_concatene % code_additionne == 0
     return code_additionne, code_concatene, mot_est_parfait
 
 
+assert codes_parfait("PAUL") == (50, 1612112, False)
+assert codes_parfait("ALAIN") == (37, 1121914, True)
