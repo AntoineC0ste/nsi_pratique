be9555b1247bc4e80857faaa8e09349991622b1a
Antoine <antoine.coste@e.rascol.net>
2025-05-20 11:37:51 +0200

    36

--- Sujets_Pratique_NSI_2025/25-NSI-36/25-NSI-36.py
+++ Sujets_Pratique_NSI_2025/25-NSI-36/25-NSI-36.py
@@ -1,3 +1,18 @@
+# Exercice 1
+def nombre_de_mots(phrase:str)->int:
+    nbr = 0
+    for c in phrase:
+        if c in (' ', '.'):
+            nbr += 1
+
+    return nbr
+
+assert  nombre_de_mots('Cet exercice est simple.') == 4
+assert nombre_de_mots('Le point d exclamation est séparé !') == 6
+assert nombre_de_mots('Combien de mots y a t il dans cette phrase ?') == 10
+assert nombre_de_mots('Fin.') == 1
+
+# Exercice 2
 class Noeud:
     def __init__(self, etiquette):
         '''Méthode constructeur pour la classe Noeud.
@@ -11,13 +26,22 @@ class Noeud:
         en préservant sa structure.'''
         if cle < self.etiquette:
             if self.gauche != None:
-                ...
+                return self.gauche.inserer(cle)
             else:
-                self.gauche = ... 
+                self.gauche = Noeud(cle)
         else:
-            ...
-                ...
+            if self.droit != None:
+                return self.droit.inserer(cle)
             else:
-                ... = Noeud(cle) 
+                self.droit = Noeud(cle) 
 
 
+arbre = Noeud(7)
+
+for cle in (3,9,1,6):
+    arbre.inserer(cle)
+
+assert arbre.gauche.etiquette == 3
+assert arbre.droit.etiquette == 9
+assert arbre.gauche.gauche.etiquette == 1
+assert arbre.gauche.droit.etiquette == 6
