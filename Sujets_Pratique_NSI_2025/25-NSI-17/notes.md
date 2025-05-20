cc7c262d888402bf43b038e7e50559ea4f81def7
Antoine <antoine.coste@e.rascol.net>
2025-05-20 09:38:15 +0200

    17

---
+++ Sujets_Pratique_NSI_2025/25-NSI-17/notes.md
@@ -0,0 +1,2 @@
+# Exercice 2
+Ne pas oublier le -1 après avoir inséré dans l'indice
--- Sujets_Pratique_NSI_2025/25-NSI-17/sujet-17.py
+++ Sujets_Pratique_NSI_2025/25-NSI-17/sujet-17.py
@@ -1,13 +1,45 @@
+# Exercice 1
+class Noeud:
+    def __init__(self, etiquette, gauche, droit):
+        self.v = etiquette
+        self.gauche = gauche
+        self.droit = droit
+
+def taille(abr:Noeud) -> int:
+    if abr == None or abr.v == None:
+        return 0
+    
+    return 1 + taille(abr.gauche) + taille(abr.droit)
+
+def hauteur(abr:Noeud) -> int:
+    if abr == None or (abr.droit == None and abr.gauche == None):
+        return 0
+    
+    return 1 + hauteur(abr.gauche) + hauteur(abr.droit)
+    
+
+a = Noeud(1, Noeud(4, None, None),
+    Noeud(0, None,
+             Noeud(7, Noeud(9, None, None), None)))
+
+assert taille(a) == 5
+assert hauteur(a) == 3, f'{hauteur(a)}'
+
+
+# Exercice 2
 def ajoute(indice, element, tab):
     '''Renvoie un nouveau tableau obtenu en insérant
     element à l'indice indice dans le tableau tab.'''
     nbre_elts = len(tab)
     tab_ins = [0] * (nbre_elts + 1)
     for i in range(indice):
-        tab_ins[i] = ... 
-    tab_ins[...] = ... 
+        tab_ins[i] = tab[i] 
+    tab_ins[indice] =  element
     for i in range(indice + 1, nbre_elts + 1):
-        tab_ins[i] = ... 
+        tab_ins[i] = tab[i-1]
     return tab_ins
 
 
+assert ajoute(1, 4, [7, 8, 9]) == [7, 4, 8, 9]
+assert ajoute(3, 4, [7, 8, 9]) == [7, 8, 9, 4]
+assert ajoute(0, 4, [7, 8, 9]) == [4, 7, 8, 9]
