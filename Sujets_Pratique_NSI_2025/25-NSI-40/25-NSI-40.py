d6e1117ea4a4a9d17813bcdc08540b1d9c856373
Antoine <antoine.coste@e.rascol.net>
2025-05-20 10:13:23 +0200

    40

--- Sujets_Pratique_NSI_2025/25-NSI-40/25-NSI-40.py
+++ Sujets_Pratique_NSI_2025/25-NSI-40/25-NSI-40.py
@@ -1,3 +1,24 @@
+# Exercice 1
+def recherche_indices_classement(elt:int, tab:list[int]) -> tuple[list[int]]:
+    inf = []
+    eq = []
+    sup = []
+    for i  in range(len(tab)):
+        if tab[i] < elt:
+            inf.append(i)
+        elif tab[i] == elt:
+            eq.append(i)
+        elif tab[i] > elt:
+            sup.append(i)
+
+    return (inf, eq, sup)
+
+assert recherche_indices_classement(3, [1, 3, 4, 2, 4, 6, 3, 0]) == ([0, 3, 7], [1, 6], [2, 4, 5])
+assert recherche_indices_classement(3, [1, 4, 2, 4, 6, 0]) == ([0, 2, 5], [], [1, 3, 4])
+assert recherche_indices_classement(3, [1, 1, 1, 1]) == ([0, 1, 2, 3], [], [])
+assert recherche_indices_classement(3, []) == ([], [], [])
+
+# Exercice 2
 resultats = {
     'Dupont': {
         'DS1': [15.5, 4],
@@ -19,18 +40,20 @@ def moyenne(nom, resultats):
     '''Renvoie la moyenne de l'élève nom, selon le dictionnaire 
     resultats. Si nom n'est pas dans le dictionnaire, 
     la fonction renvoie None.'''
-    if nom in ...: 
+    if nom in resultats.keys(): 
         notes = resultats[nom]
-        if ...: # pas de notes 
+        if notes == {}: # pas de notes 
             return 0
-        total_points = ... 
-        total_coefficients = ... 
-        for ...  in notes.values(): 
+        total_points = 0 
+        total_coefficients = 0
+        for valeurs  in notes.values(): 
             note, coefficient = valeurs
-            total_points = total_points + ... * coefficient 
-            ... = ... + coefficient 
-        return round( ... / total_coefficients, 1 ) 
+            total_points = total_points + note * coefficient 
+            total_coefficients = total_coefficients + coefficient 
+        return round( total_points / total_coefficients, 1 ) 
     else:
         return None
 
+assert moyenne("Dupont", resultats) == 14.5
+assert moyenne("Durand", resultats) == 8.5
 
