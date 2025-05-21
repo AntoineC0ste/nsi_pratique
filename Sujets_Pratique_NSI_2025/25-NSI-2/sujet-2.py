d4ae65f8aec0d229ab2f5be8c24c6c003aaccd32
Antoine <antoine.coste@e.rascol.net>
2025-05-21 22:10:20 +0200

    2

--- Sujets_Pratique_NSI_2025/25-NSI-2/sujet-2.py
+++ Sujets_Pratique_NSI_2025/25-NSI-2/sujet-2.py
@@ -1,3 +1,20 @@
+# Exercice 1
+def max_et_indice(tab:list[int]) -> tuple[int]:
+    imax = 0
+    for i in range(len(tab)):
+        if tab[i] > tab[imax]:
+            imax = i
+    
+    return (tab[imax], imax)
+
+
+assert max_et_indice([1, 5, 6, 9, 1, 2, 3, 7, 9, 8]) == (9,3)
+assert max_et_indice([-2]) == (-2, 0)
+assert max_et_indice([-1, -1, 3, 3, 3]) == (3,2)
+assert max_et_indice([1, 1, 1, 1]) == (1, 0)
+
+
+# Exercice 2
 def est_un_ordre(tab):
     '''
     Renvoie True si tab est de longueur n et contient tous les
@@ -5,12 +22,12 @@ def est_un_ordre(tab):
     '''
     n = len(tab)
     # les entiers vus lors du parcours
-    vus = ... 
+    vus = []
 
     for x in tab:
-        if x < ... or x >... or ...: 
+        if x < 1 or x > n or x in vus: 
             return False
-        ... .append(...) 
+        vus.append(x) 
     return True
 
 def nombre_points_rupture(ordre):
@@ -19,18 +36,23 @@ def nombre_points_rupture(ordre):
     un ordre de gènes de chromosome
     '''
     # on vérifie que ordre est un ordre de gènes
-    assert ... 
+    assert est_un_ordre(ordre)
     n = len(ordre)
     nb = 0
-    if ordre[...] != 1: # le premier n'est pas 1 
+    if ordre[0] != 1: # le premier n'est pas 1 
         nb = nb + 1
     i = 0
-    while i < ...: 
-        if ... not in [-1, 1]: # l'écart n'est pas 1 
+    while i < n-1: 
+        if ordre[i+1]-ordre[i] not in [-1, 1]: # l'écart n'est pas 1 
             nb = nb + 1
         i = i + 1
-    if ordre[i] != ...: # le dernier n'est pas n 
+    if ordre[i] != n: # le dernier n'est pas n 
         nb = nb + 1
     return nb
 
-
+assert not est_un_ordre([1, 6, 2, 8, 3, 7])
+assert est_un_ordre([5, 4, 3, 6, 7, 2, 1, 8, 9])
+assert nombre_points_rupture([5, 4, 3, 6, 7, 2, 1, 8, 9]) == 4
+assert nombre_points_rupture([1, 2, 3, 4, 5]) == 0
+assert nombre_points_rupture([1, 6, 2, 8, 3, 7, 4, 5]) == 7
+assert  nombre_points_rupture([2, 1, 3, 4]) == 2
