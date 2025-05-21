d7582b778423dd3a9f507b11172af336faa221f5
Antoine <antoine.coste@e.rascol.net>
2025-05-21 21:22:13 +0200

    3

--- Sujets_Pratique_NSI_2025/25-NSI-3/sujet-3.py
+++ Sujets_Pratique_NSI_2025/25-NSI-3/sujet-3.py
@@ -1,14 +1,30 @@
+# Exercice 1
+def fibonnaci(n:int) -> int:
+    if n == 1 or n == 2:
+        return 1
+    else:
+        return fibonnaci(n-1) + fibonnaci(n-2)
+
+assert fibonnaci(1) == 1
+assert fibonnaci(2) == 1
+assert fibonnaci(25) == 75025
+
+
+# Exercice 2
 def eleves_du_mois(eleves, notes):
     note_maxi = 0
-    meilleurs_eleves = ... 
+    meilleurs_eleves = []
 
-    for i in range(...): 
-        if notes[i] == ...: 
-            meilleurs_eleves.append(...) 
+    for i in range(len(eleves)): 
+        if notes[i] == note_maxi: 
+            meilleurs_eleves.append(eleves[i]) 
         elif notes[i] > note_maxi:
-            note_maxi = ... 
-            meilleurs_eleves = [...] 
+            note_maxi = notes[i]
+            meilleurs_eleves = [eleves[i]] 
 
     return (note_maxi, meilleurs_eleves)
 
 
+eleves_nsi = ['a','b','c','d','e','f','g','h','i','j']
+notes_nsi = [30, 40, 80, 60, 58, 80, 75, 80, 60, 24]
+assert eleves_du_mois(eleves_nsi, notes_nsi) == (80, ['c', 'f', 'h'])
