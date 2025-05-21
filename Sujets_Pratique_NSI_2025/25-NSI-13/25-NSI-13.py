865cf77c88bb65e8f451259efc7d27d68cf5f67b
Antoine <antoine.coste@e.rascol.net>
2025-05-21 22:17:57 +0200

    13

--- Sujets_Pratique_NSI_2025/25-NSI-13/25-NSI-13.py
+++ Sujets_Pratique_NSI_2025/25-NSI-13/25-NSI-13.py
@@ -1,3 +1,18 @@
+# Exercice 1
+def recherche(elt:int, tab:list) -> int:
+    if not elt in tab:
+        return None
+
+    for i in range(len(tab)):
+        if tab[i] == elt:
+            return i
+
+assert recherche(1, [2, 3, 4]) == None
+assert recherche(1, [10, 12, 1, 56]) == 2
+assert recherche(50, [1, 50, 1])
+assert recherche(15, [8, 9, 10, 15]) == 3
+
+# Exercice 2
 def insere(tab, a):
     """
     Insère l'élément a (int) dans le tableau tab (list)
@@ -7,10 +22,13 @@ def insere(tab, a):
     tab_a = [ a ] + tab # nouveau tableau contenant a 
                         # suivi des éléments de tab
     i = 0
-    while i < ... and a > ...: 
-        tab_a[i] = ... 
+    while i < len(tab) and a > tab[i]: 
+        tab_a[i] = tab_a[i+1]
         tab_a[i+1] = a
-        i = ... 
+        i = i + 1
     return tab_a
 
-
+assert insere([1, 2, 4, 5], 3) == [1,2,3,4,5]
+assert insere([1, 2, 7, 12, 14, 25], 30) == [1, 2, 7, 12, 14, 25, 30]
+assert insere([2, 3, 4], 1) == [1,2,3,4]
+assert insere([], 1) == [1]
