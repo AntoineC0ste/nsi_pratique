2695c8f26b1178069bd3f27fff3dcc4d53ab0846
Antoine <antoine.coste@e.rascol.net>
2025-05-21 22:31:44 +0200

    18

--- Sujets_Pratique_NSI_2025/25-NSI-18/sujet-18.py
+++ Sujets_Pratique_NSI_2025/25-NSI-18/sujet-18.py
@@ -1,18 +1,39 @@
+# Exercice 1
+def moyenne(tab:list) -> float:
+    num = 0
+    denum = len(tab)
+
+    for elt in tab:
+        num += elt
+    
+    return num/denum
+
+assert moyenne([1])
+assert  moyenne([1, 2, 3, 4, 5, 6, 7]) == 4.0
+assert moyenne([1, 2]) == 1.5
+
+
+
+# Exercice 2
 def dichotomie(tab, x):
     """applique une recherche dichotomique pour déterminer
     si x est dans le tableau trié tab.
     La fonction renvoie True si tab contient x et False sinon"""
 
     debut = 0
-    fin = ... 
+    fin = len(tab) - 1
     while debut <= fin:
-        m = ... 
+        m = len(tab[debut:fin])
         if x == tab[m]:
-            return ... 
+            return True
         if x > tab[m]:
-            debut = ... 
+            debut = m + 1
         else:
-            fin = ... 
+            fin = m - 1
     return False
 
 
+assert  dichotomie([15, 16, 18, 19, 23, 24, 28, 29, 31, 33], 28)
+assert not  dichotomie([15, 16, 18, 19, 23, 24, 28, 29, 31, 33], 27)
+assert not dichotomie([15, 16, 18, 19, 23, 24, 28, 29, 31, 33], 1)
+assert not  dichotomie([], 28)
