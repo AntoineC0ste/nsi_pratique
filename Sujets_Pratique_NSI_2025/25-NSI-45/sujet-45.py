771f19974c32d6f801e1c8013191a4315194a9f3
Antoine <antoine.coste@e.rascol.net>
2025-05-21 17:14:00 +0200

    45

--- Sujets_Pratique_NSI_2025/25-NSI-45/sujet-45.py
+++ Sujets_Pratique_NSI_2025/25-NSI-45/sujet-45.py
@@ -1,3 +1,24 @@
+# Exercice 1
+def correspond(mot:str, mot_a_trous:str) -> bool:
+    if len(mot) != len(mot_a_trous):
+        return False
+    
+    mot_sans_trou = ''
+
+    for i in range(len(mot)):
+        if mot_a_trous[i] == '*':
+            mot_sans_trou += mot[i]
+        else:
+            mot_sans_trou += mot_a_trous[i]
+
+    return mot == mot_sans_trou
+
+assert correspond('INFORMATIQUE', 'INFO*MA*IQUE')
+assert not correspond('AUTOMATIQUE', 'INFO*MA*IQUE')
+assert not  correspond('STOP', 'S*')
+assert correspond('AUTO', '*UT*')
+
+# Exercice 2
 def est_cyclique(plan):
     '''Prend en paramètre un dictionnaire `plan` correspondant à 
     un plan d'envoi de messages (ici entre les personnes A, B, C,
@@ -5,13 +26,17 @@ def est_cyclique(plan):
     Renvoie True si le plan d'envoi de messages est cyclique et 
     False sinon.'''
     expediteur = 'A'
-    destinataire = plan[...] 
+    destinataire = plan[expediteur] 
     nb_destinataires = 1
 
     while destinataire != expediteur:
-        destinataire = ... 
-        nb_destinataires = ... 
+        destinataire = plan[destinataire]
+        nb_destinataires = nb_destinataires + 1
 
-    return nb_destinataires == ... 
+    return nb_destinataires == len(plan)
 
 
+assert not est_cyclique({'A':'E','F':'A','C':'D','E':'B','B':'F','D':'C'})
+assert est_cyclique({'A':'E','F':'C','C':'D','E':'B','B':'F','D':'A'})
+assert est_cyclique({'A':'B','F':'C','C':'D','E':'A','B':'F','D':'E'})
+assert not est_cyclique({'A':'B','F':'A','C':'D','E':'C','B':'F','D':'E'})
